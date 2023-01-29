1d5cf30afd2170bf8811225ff4929b6f65629bf3
Roland Kajatin <roland.kajatin@gmail.com>
2023-01-29 17:40:54 +0100

    working on projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,21 @@
+<script>
+	import Project from './Project.svelte';
+    import { projects } from '../projects.json';
+</script>
+
 <h1>Coming soon...</h1>
+<div class="projects">
+	{#each projects as project}
+		<Project {project} />
+	{/each}
+</div>
+
+<style>
+	.projects {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 2rem;
+		width: 100%;
+	}
+</style>
