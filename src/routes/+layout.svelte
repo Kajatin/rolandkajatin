0e7a09af8382ccfa1d8022ea6227e0329ee0fdbb
Roland Kajatin <roland.kajatin@gmail.com>
2023-01-29 12:29:56 +0100

    WIP header + footer + github corner badge

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,32 +14,33 @@
 </div>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap');
-    @import './variables.css';
+	@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap');
+	@import './variables.css';
 
-    .app {
-        display: flex;
-        flex-direction: column;
-        min-height: 100vh;
-    }
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
 
-    :global(body) {
-        margin: 0;
-        font-family: 'Roboto Condensed', sans-serif;
-        background: rgb(251, 250, 241);
-    }
+	:global(body) {
+		margin: 0;
+		font-family: 'Roboto Condensed', sans-serif;
+		background: var(--page-background);
+	}
 
-    main {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        flex-grow: 1;
-    }
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		flex-grow: 1;
+		padding: 2rem;
+	}
 
-    /* detect dark mode preference */
-    /* @media (prefers-color-scheme: dark) {
+	/* detect dark mode preference */
+	/* @media (prefers-color-scheme: dark) {
         :global(body) {
             background: #111;
             color: #ccc;
