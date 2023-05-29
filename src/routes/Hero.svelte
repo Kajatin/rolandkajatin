a4b5f6ea09e9077f4d77c0f66745b82c30786967
Roland Kajatin <roland.kajatin@gmail.com>
2023-05-29 23:14:35 +0200

    loading spinner

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import P5, { type Sketch } from 'p5-svelte';
+	import LoaderSpinner from './LoaderSpinner.svelte';
 
-	let img;
-	let imageLoaded = false;
 	let sketchParsed: Sketch | null = null;
 
 	let sketch = `(p5) => {
@@ -128,9 +127,6 @@
 	};`;
 
 	onMount(async () => {
-		img = new Image();
-		img.onload = () => imageLoaded = true;
-		img.src = '/roland.png';
 		sketchParsed = (0, eval)(sketch);
 	});
 
@@ -181,7 +177,7 @@
 		</div>
 	</div>
 {:else}
-  <div class="spinner"></div>
+  <LoaderSpinner />
 {/if}
 
 <style>
@@ -229,26 +225,6 @@
 		border-bottom: 0.1rem solid var(--accent-secondary);
 	}
 
-	.spinner {
-		position: relative;
-		width: 4rem;
-		height: 4rem;
-		animation: spin 1.3s cubic-bezier(0.33, 0, 0.67, 1) infinite;
-		margin: 10rem auto;
-	}
-
-	.spinner::before {
-		content: "";
-		position: absolute;
-		top: -0.5rem;
-		left: -0.5rem;
-		right: -0.5rem;
-		bottom: -0.5rem;
-		background: linear-gradient(25deg, var(--accent-primary), var(--accent-secondary));
-		border-radius: 1rem;
-		z-index: -1;
-	}
-
 	@keyframes spin {
 		0% { transform: rotate(0deg); }
 		80% { transform: rotate(360deg); }
@@ -266,10 +242,6 @@
 
 		a:hover {
 			border-bottom: 0.1rem solid var(--accent-secondary-dark);
-		}
-
-		.spinner::before {
-			background: linear-gradient(25deg, var(--accent-primary-dark), var(--accent-secondary-dark));
 		}
 	}
 
---
+++ src/routes/LoaderSpinner.svelte
@@ -0,0 +1,51 @@
+<div class="spinner"></div>
+
+<style>
+	.spinner {
+		position: relative;
+		width: 4rem;
+		height: 4rem;
+		animation: spin 1.3s cubic-bezier(0.33, 0, 0.67, 1) infinite;
+		margin: 10rem auto;
+	}
+
+	.spinner::before {
+		content: "";
+		position: absolute;
+		top: -0.5rem;
+		left: -0.5rem;
+		right: -0.5rem;
+		bottom: -0.5rem;
+		background: linear-gradient(25deg, var(--accent-primary), var(--accent-secondary));
+		border-radius: 1rem;
+		z-index: -1;
+	}
+
+	.spinner::after {
+		content: "";
+		position: absolute;
+		top: 0.1rem;
+		left: 0.1rem;
+		right: 0.1rem;
+		bottom: 0.1rem;
+		background: var(--page-background);
+		border-radius: 100%;
+		z-index: -1;
+	}
+
+	@keyframes spin {
+		0% { transform: rotate(0deg); }
+		80% { transform: rotate(360deg); }
+		100% { transform: rotate(360deg); }
+	}
+
+	@media (prefers-color-scheme: dark) {
+		.spinner::before {
+			background: linear-gradient(25deg, var(--accent-primary-dark), var(--accent-secondary-dark));
+		}
+
+		.spinner::after {
+			background: var(--page-background-dark);
+		}
+	}
+</style>
